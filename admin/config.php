fbf304a409dae82b643aa4d0ee5b4646d8b007d1
zkimyes <153033541@qq.com>
2017-06-03 08:48:49 +0800

    项目初始化

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', 'G:/php_projects/thunder-link-new/admin/');
+define('DIR_SYSTEM', 'G:/php_projects/thunder-link-new/system/');
+define('DIR_IMAGE', 'G:/php_projects/thunder-link-new/image/');
+define('DIR_LANGUAGE', 'G:/php_projects/thunder-link-new/admin/language/');
+define('DIR_TEMPLATE', 'G:/php_projects/thunder-link-new/admin/view/template/');
+define('DIR_CONFIG', 'G:/php_projects/thunder-link-new/system/config/');
+define('DIR_CACHE', 'G:/php_projects/thunder-link-new/system/storage/cache/');
+define('DIR_DOWNLOAD', 'G:/php_projects/thunder-link-new/system/storage/download/');
+define('DIR_LOGS', 'G:/php_projects/thunder-link-new/system/storage/logs/');
+define('DIR_MODIFICATION', 'G:/php_projects/thunder-link-new/system/storage/modification/');
+define('DIR_UPLOAD', 'G:/php_projects/thunder-link-new/system/storage/upload/');
+define('DIR_CATALOG', 'G:/php_projects/thunder-link-new/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'new_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', 'G:/php_projects/thunder-link-new/catalog/');
+define('DIR_SYSTEM', 'G:/php_projects/thunder-link-new/system/');
+define('DIR_IMAGE', 'G:/php_projects/thunder-link-new/image/');
+define('DIR_LANGUAGE', 'G:/php_projects/thunder-link-new/catalog/language/');
+define('DIR_TEMPLATE', 'G:/php_projects/thunder-link-new/catalog/view/theme/');
+define('DIR_CONFIG', 'G:/php_projects/thunder-link-new/system/config/');
+define('DIR_CACHE', 'G:/php_projects/thunder-link-new/system/storage/cache/');
+define('DIR_DOWNLOAD', 'G:/php_projects/thunder-link-new/system/storage/download/');
+define('DIR_LOGS', 'G:/php_projects/thunder-link-new/system/storage/logs/');
+define('DIR_MODIFICATION', 'G:/php_projects/thunder-link-new/system/storage/modification/');
+define('DIR_UPLOAD', 'G:/php_projects/thunder-link-new/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'new_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
