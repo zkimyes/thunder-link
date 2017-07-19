ae6a4aa287844501ef9360e3381c5a9d1ae30656
zkimyes <zkimyes@zkimyesdeMacBook-Air.local>
2017-07-20 00:50:38 +0800

    ddd

--- admin/controller/configuration/category.php
+++ admin/controller/configuration/category.php
@@ -1,7 +1,98 @@
 <?php
+// config 目录
 
 class ControllerConfigurationCategory extends Controller{
     public function index(){
-        echo 111;
+        $this->document->setTitle('asdasd');
+        $this->getList();
+    }
+
+    public function delete(){
+        $this->load->model('solution/category');
+        if (isset($this->request->post['selected'])) {
+            $this->model_solution_category->delt($this->request->post['selected']);
+			$this->response->jsonOutput([
+                'status'=>'1',
+                'info'=>'success'
+            ]);
+		}else{
+            $this->response->jsonOutput([
+                'status'=>'0',
+                'info'=>'no item to delete'
+            ]);
+        }
+    }
+
+
+    public function add(){
+        $this->load->model('solution/category');
+        $data['submit_url'] = $this->url->link('solution/category/add');
+        if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest"){ 
+             $post = $this->request->post;
+             $rs = $this->model_solution_category->add($post);
+             $this->response->jsonOutput($rs);
+        }else{ 
+            $this->form($data);
+        };
+    }
+
+    public function update(){
+        $this->load->model('solution/category');
+        $data['submit_url'] = $this->url->link('solution/category/update');
+        if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest"){ 
+             $post = $this->request->post;
+             $rs = $this->model_solution_category->update($post);
+             $this->response->jsonOutput($rs);
+        }else{ 
+            $id = $this->request->get['id'];
+            if(!empty($id)){
+                $category = $this->model_solution_category->find($id);
+            }
+            $data['category'] = $category;
+            $this->form($data);
+        };
+    }
+
+
+    protected function form($data){
+        $data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer'); 
+        $data['back_url'] = $this->url->link('solution/category/index');
+        $data['token'] = $this->session->data['token'];
+        $this->response->setOutput($this->load->view('solution/category_form', $data));
+    }
+    
+    
+    protected function getList() {
+        $token = $this->session->data['token'];
+        $url = "";
+        $data['url'] = $this->url;
+        $data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => 'Configuration Category',
+			'href' => $this->url->link('sale/order', 'token=' . $this->session->data['token'] . $url, true)
+		);
+        $data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+        $data['add_url'] = $this->url->link('solution/category/add');
+        $data['update_url'] = $this->url->link('solution/category/update');
+        $data['delt_url'] = $this->url->link('solution/category/delete');
+
+        $this->load->model('solution/category');
+        $categorys = $this->model_solution_category->getList();
+
+        $data['lists'] = json_encode($categorys);
+
+        $data['token'] = $token;
+        $this->response->setOutput($this->load->view('configuration/category_list', $data));
     }
 }
---
+++ admin/view/template/configuration/category_form.tpl
@@ -0,0 +1,88 @@
+<?php echo $header; ?>
+<?php echo $column_left; ?>
+<div id="content">
+    <div class="page-header">
+        <div class="container-fluid">
+            <h1>
+                Solution Category
+            </h1>
+            <ul class="breadcrumb">
+                <li>
+                    <a></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>title</label>
+                            <input type="text" v-model="title" class="form-control" placeholder="title">
+                            <div v-if="title === ''" class="help-block text-danger">title 不能为空</div>
+                        </div>
+                        <div class="form-group">
+                            <label>meta keyword</label>
+                            <input type="text" v-model="meta_keyword" class="form-control" placeholder="meta keyword">
+                        </div>
+                        <div class="form-group">
+                            <label>meta description</label>
+                            <textarea v-model="meta_desc" class="form-control" placeholder="meta description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>link url</label>
+                            <input type="text" v-model="url" class="form-control" placeholder="link url">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 text-left">
+                        <button @click="submit()" class="btn btn-primary">提交</button>
+                        <a class="btn btn-default" href="{{back_url|raw}}&token={{token}}">取消</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    Vue.config.devtools = true
+    var category = new Vue({
+        el:'#content',
+        data:{
+            id:'{{category.id}}',
+            title:'{{category.title}}',
+            meta_keyword:'{{category.meta_keyword}}',
+            meta_desc:'{{category.meta_desc}}',
+            url:'{{category.url}}'
+        },
+        methods:{
+            submit(){
+                let data = {
+                    id:this.id,
+                    title:this.title,
+                    meta_keyword:this.meta_keyword,
+                    meta_desc:this.meta_desc,
+                    url:this.url
+                }
+
+                if(data.title == ""){
+                    return layer.msg('标题不能为空');
+                }
+
+                if(data.title == ""){
+                    return layer.msg('标题不能为空');
+                }
+
+                $.post('{{submit_url|raw}}&token={{token}}',data,function(res){
+                    if(res){
+                        location.href="{{back_url|raw}}&token={{token}}";
+                    }
+                },'json')
+            }
+        }
+    });
+</script>
+<?php echo $footer; ?>
