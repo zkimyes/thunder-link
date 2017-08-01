3189a31d468a637b68975b8a20b7baf41cb3211c
zkimyes <153033541@qq.com>
2017-08-01 23:08:29 +0800

    顶顶顶

--- admin/controller/solution/category.php
+++ admin/controller/solution/category.php
@@ -4,7 +4,7 @@
 * Undocumented class
 */
 class ControllerSolutionCategory extends Controller{
-
+    
     public function index(){
         $this->load->language('sale/order');
         
@@ -14,44 +14,52 @@ class ControllerSolutionCategory extends Controller{
         
         $this->getList();
     }
-
+    
     public function delete(){
         $this->load->model('solution/category');
         if (isset($this->request->post['selected'])) {
             $this->model_solution_category->delt($this->request->post['selected']);
-			$this->response->jsonOutput([
-                'status'=>'1',
-                'info'=>'success'
-            ]);
-		}else{
             $this->response->jsonOutput([
-                'status'=>'0',
-                'info'=>'no item to delete'
+            'status'=>'1',
+            'info'=>'success'
+            ]);
+        }else{
+            $this->response->jsonOutput([
+            'status'=>'0',
+            'info'=>'no item to delete'
             ]);
         }
     }
-
-
+    
+    
     public function add(){
         $this->load->model('solution/category');
         $data['submit_url'] = $this->url->link('solution/category/add');
-        if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest"){ 
-             $post = $this->request->post;
-             $rs = $this->model_solution_category->add($post);
-             $this->response->jsonOutput($rs);
-        }else{ 
+        if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest"){
+            $post = $this->request->post;
+            $rs = $this->model_solution_category->add($post);
+            $this->response->jsonOutput($rs);
+        }else{
             $this->form($data);
         };
     }
-
+    
     public function update(){
         $this->load->model('solution/category');
+        $data['breadcrumbs'][] = [
+        'text'=>'Home',
+        'href'=>$this->url->link('commom/dashboard','token='.$this->session->data['token'])
+        ];
+        $data['breadcrumbs'][] = [
+        'text'=>'Solution Category Update',
+        'href'=>''
+        ];
         $data['submit_url'] = $this->url->link('solution/category/update');
-        if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest"){ 
-             $post = $this->request->post;
-             $rs = $this->model_solution_category->update($post);
-             $this->response->jsonOutput($rs);
-        }else{ 
+        if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest"){
+            $post = $this->request->post;
+            $rs = $this->model_solution_category->update($post);
+            $this->response->jsonOutput($rs);
+        }else{
             $id = $this->request->get['id'];
             if(!empty($id)){
                 $category = $this->model_solution_category->find($id);
@@ -60,12 +68,12 @@ class ControllerSolutionCategory extends Controller{
             $this->form($data);
         };
     }
-
-
+    
+    
     protected function form($data){
         $data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer'); 
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
         $data['back_url'] = $this->url->link('solution/category/index');
         $data['token'] = $this->session->data['token'];
         $this->response->setOutput($this->load->view('solution/category_form', $data));
@@ -77,29 +85,29 @@ class ControllerSolutionCategory extends Controller{
         $url = "";
         $data['url'] = $this->url;
         $data['breadcrumbs'] = array();
-
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], true)
-		);
-
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('sale/order', 'token=' . $this->session->data['token'] . $url, true)
-		);
+        
+        $data['breadcrumbs'][] = array(
+        'text' => $this->language->get('text_home'),
+        'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], true)
+        );
+        
+        $data['breadcrumbs'][] = array(
+        'text' => $this->language->get('heading_title'),
+        'href' => $this->url->link('sale/order', 'token=' . $this->session->data['token'] . $url, true)
+        );
         $data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer');
-
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
+        
         $data['add_url'] = $this->url->link('solution/category/add');
         $data['update_url'] = $this->url->link('solution/category/update');
         $data['delt_url'] = $this->url->link('solution/category/delete');
-
+        
         $this->load->model('solution/category');
         $categorys = $this->model_solution_category->getList();
-
+        
         $data['lists'] = json_encode($categorys);
-
+        
         $data['token'] = $token;
         $this->response->setOutput($this->load->view('solution/category_list', $data));
     }
--- admin/view/template/solution/category_form.tpl
+++ admin/view/template/solution/category_form.tpl
@@ -23,20 +23,20 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>title</label>
+                            <label>Title</label>
                             <input type="text" v-model="title" class="form-control" placeholder="title">
                             <div v-if="title === ''" class="help-block text-danger">title 不能为空</div>
                         </div>
                         <div class="form-group">
-                            <label>meta keyword</label>
+                            <label>Meta Keyword</label>
                             <input type="text" v-model="meta_keyword" class="form-control" placeholder="meta keyword">
                         </div>
                         <div class="form-group">
-                            <label>meta description</label>
+                            <label>Meta Description</label>
                             <textarea v-model="meta_desc" class="form-control" placeholder="meta description"></textarea>
                         </div>
                         <div class="form-group">
-                            <label>link url</label>
+                            <label>Link Url</label>
                             <input type="text" v-model="url" class="form-control" placeholder="link url">
                         </div>
                     </div>
