e4ee718126e752fd96b8cf64542abaa740a25e4e
zkimyes <zkimyes@zkimyesdeMacBook-Air.local>
2017-09-25 02:03:08 +0800

    点点滴滴

---
+++ catalog/controller/product/hotsale.php
@@ -0,0 +1,13 @@
+<?php
+class ControllerProductHotsale extends Controller {
+	public function index() {
+			$data['column_left'] = $this->load->controller('common/column_left');
+			$data['column_right'] = $this->load->controller('common/column_right');
+			$data['content_top'] = $this->load->controller('common/content_top');
+			$data['content_bottom'] = $this->load->controller('common/content_bottom');
+			$data['footer'] = $this->load->controller('common/footer');
+			$data['header'] = $this->load->controller('common/header');
+
+			$this->response->setOutput($this->load->view('product/hotsale', $data));
+	}
+}
